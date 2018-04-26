3ed7b25dfbf64a325cfb28c3578bb8a4466671eb
DESKTOP-84KESM8\shigh <shigha25@gmail.com>
2018-04-25 20:51:05 -0500

    Camera01

---
+++ topdownZombie/Assets/Camera/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public float smoothing = 5f; // delay of cammera movemen
+
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+    void FixedUpdate()
+    {
+        Vector3 targetCamPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
+}
